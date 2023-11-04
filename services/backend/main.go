af4093106d1e0921518a31ac993e36d538216402
Revon <revon@Revons-MBP.attlocal.net>
2023-11-04 16:25:56 -0700

    add prometheus metrics

--- services/backend/main.go
+++ services/backend/main.go
@@ -4,11 +4,20 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var counter = 0
+
 func main() {
+
+	prometheus.MustRegister(pingCounter)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		pingCounter.Inc()
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
@@ -19,5 +28,23 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	r.GET("/metrics", prometheusHandler())
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "ping_request_count",
+		Help: "No of request handled by ping handler",
+	},
+)
+
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
 }
